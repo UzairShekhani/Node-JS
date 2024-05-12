eb421a39f8dc267f93eb80b29f753b771d6bd50b
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-12 10:23:37 +0500

    write file completed

---
+++ script.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+// writefile
+// copyFile
+// appendFile
+// unlink
+// rename
+
+
+fs.writeFile("hey.txt","hey hello kaisa ho ", function(err){
+    if(err)console.log(err)
+        else console.log("done")
+    
+})
