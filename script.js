4c53bb3d22c3c2787fcf5839a1b0a859bc1e5e12
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-12 10:33:02 +0500

    rename file completed

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ const fs = require('fs');
 // rename
 
 
-fs.writeFile("hey.txt","hey hello kaisa ho ", function(err){
-    if(err)console.log(err)
-        else console.log("done")
+// fs.writeFile("hey.txt","hey hello kaisa ho ", function(err){
+//     if(err)console.log(err)
+//         else console.log("done")
     
-})
+// })
 
 
-fs.appendFile("hey.txt", "mera name uzair ha",function(err){
-    if(err)console.log(err)
-        else console.log("done")
+// fs.appendFile("hey.txt", "mera name uzair ha",function(err){
+//     if(err)console.log(err)
+//         else console.log("done")
+// })
+
+
+fs.rename("hey.txt","hello.txt", function (err) {
+    if (err) console.log(err)
+        else console.log("done");
 })
