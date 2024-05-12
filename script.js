b518dcac1e8d90ef692e17eb16786c4f8a7e7d35
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-12 11:07:41 +0500

    done

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ const fs = require('fs');
 // });
 
 
-fs.rm("./index",{recursive: true}, function (err) {
-    if (err) console.error(err);
-    else console.log("done");
-});
+// fs.rm("./index",{recursive: true}, function (err) {
+//     if (err) console.error(err);
+//     else console.log("done");
+// });
