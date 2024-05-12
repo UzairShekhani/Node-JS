48e1d8daa09c24c71a7d76549e31c1aa53bdf340
░░░░░U░Z░A░I░R░░░░ <uzairshekhani2017@gmail.com>
2024-05-12 10:27:52 +0500

    append file complete

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ fs.writeFile("hey.txt","hey hello kaisa ho ", function(err){
         else console.log("done")
     
 })
+
+
+fs.appendFile("hey.txt", "mera name uzair ha",function(err){
+    if(err)console.log(err)
+        else console.log("done")
+})
